fab08b022601ccbb72d80ccb8d8bd16cb8e577e8
ekene-ezedi <ezedi.eo.ekene@gmail.com>
2021-04-20 13:40:54 +0100

    add Profile

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //connect to db
-mongoose.connect(process.env.LOCAL_DB, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true});
+mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true});
 
 const db = mongoose.connection;
 
